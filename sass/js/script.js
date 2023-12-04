d03c632a182293dc179e651feb31f74032d2cbf4
Patrick Kelly <patdkelly5@gmail.com>
2023-12-04 15:42:13 +1100

    Testing mobile version

---
+++ sass/js/script.js
@@ -0,0 +1,21 @@
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top of page
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
