51ea5106b14742cca88dce1eb19a86aeb4d2413e
Patrick Kelly <patdkelly5@gmail.com>
2023-12-05 20:38:03 +1100

    Upload of finished gallery. Testing for mobile

--- sass/js/script.js
+++ sass/js/script.js
@@ -1,21 +1,34 @@
-const allLinks = document.querySelectorAll("a:link");
+// Smooth scroll for anchor links
+const allLinks = document.querySelectorAll(".container a:link");
 
-allLinks.forEach(function (link) {
+allLinks.forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
     console.log(href);
 
     // Scroll back to top of page
-    if (href === "#")
+    if (href === "#") {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
+    }
 
+    // Smooth scroll to other sections
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
+
+// Slide navigation
+document.getElementById("next").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").appendChild(lists[0]);
+};
+document.getElementById("prev").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").prepend(lists[lists.length - 1]);
+};
