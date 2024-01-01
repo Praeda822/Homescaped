28b28a989533348327d5bf313fdd61dd0014794b
Patrick Kelly <patdkelly5@gmail.com>
2024-01-01 14:09:21 +1100

    Fixed form 405. Added client-side JS form validation | testing for functionality

--- sass/js/script.js
+++ sass/js/script.js
@@ -25,38 +25,38 @@ document.getElementById("prev").onclick = function () {
 
 //Form validation
 
-// document.querySelector(".form").addEventListener("submit", function (event) {
-//   let isFormValid = true;
+document.querySelector(".form").addEventListener("submit", function (event) {
+  let isFormValid = true;
 
-//   // Define validation functions
-//   const isEmpty = (input) => !input.value.trim();
-//   const isEmailValid = (email) => /\S+@\S+\.\S+/.test(email);
-//   const isPhoneValid = (phone) => /^\d{10}$/.test(phone);
+  // Define validation functions
+  const isEmpty = (input) => !input.value.trim();
+  const isEmailValid = (email) => /\S+@\S+\.\S+/.test(email);
+  const isPhoneValid = (phone) => /^\d{10}$/.test(phone);
 
-//   // Object mapping inputs to their validators
-//   const validators = {
-//     name: isEmpty,
-//     phone: isPhoneValid,
-//     email: isEmailValid,
-//     suburb: isEmpty,
-//     services: isEmpty,
-//     message: isEmpty,
-//   };
+  // Object mapping inputs to their validators
+  const validators = {
+    name: isEmpty,
+    phone: isPhoneValid,
+    email: isEmailValid,
+    suburb: isEmpty,
+    services: isEmpty,
+    message: isEmpty,
+  };
 
-//   Object.keys(validators).forEach((key) => {
-//     const input = document.getElementById(key);
-//     const isValid =
-//       validators[key] === isEmpty
-//         ? !validators[key](input)
-//         : validators[key](input.value);
+  Object.keys(validators).forEach((key) => {
+    const input = document.getElementById(key);
+    const isValid =
+      validators[key] === isEmpty
+        ? !validators[key](input)
+        : validators[key](input.value);
 
-//     if (!isValid) {
-//       console.error(key + " is invalid.");
-//       isFormValid = false;
-//     }
-//   });
+    if (!isValid) {
+      console.error(key + " is invalid.");
+      isFormValid = false;
+    }
+  });
 
-//   if (!isFormValid) {
-//     event.preventDefault();
-//   }
-// });
+  if (!isFormValid) {
+    event.preventDefault();
+  }
+});
