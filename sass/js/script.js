42ee5f3b21cc90dca65d2459149c56a9f316f125
Patrick Kelly <patdkelly5@gmail.com>
2024-01-01 14:06:03 +1100

    Testing form validation | fucking thing keeps 405'ing

--- sass/js/script.js
+++ sass/js/script.js
@@ -22,3 +22,41 @@ document.getElementById("prev").onclick = function () {
   let lists = document.querySelectorAll(".item");
   document.getElementById("slide").prepend(lists[lists.length - 1]);
 };
+
+//Form validation
+
+// document.querySelector(".form").addEventListener("submit", function (event) {
+//   let isFormValid = true;
+
+//   // Define validation functions
+//   const isEmpty = (input) => !input.value.trim();
+//   const isEmailValid = (email) => /\S+@\S+\.\S+/.test(email);
+//   const isPhoneValid = (phone) => /^\d{10}$/.test(phone);
+
+//   // Object mapping inputs to their validators
+//   const validators = {
+//     name: isEmpty,
+//     phone: isPhoneValid,
+//     email: isEmailValid,
+//     suburb: isEmpty,
+//     services: isEmpty,
+//     message: isEmpty,
+//   };
+
+//   Object.keys(validators).forEach((key) => {
+//     const input = document.getElementById(key);
+//     const isValid =
+//       validators[key] === isEmpty
+//         ? !validators[key](input)
+//         : validators[key](input.value);
+
+//     if (!isValid) {
+//       console.error(key + " is invalid.");
+//       isFormValid = false;
+//     }
+//   });
+
+//   if (!isFormValid) {
+//     event.preventDefault();
+//   }
+// });
